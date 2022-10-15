a7b746d7e2d3fda109c1edcc5b8fbb8125ef5e1d
Hernan Gaston <hernangaston@gmail.com>
2022-10-15 08:34:17 -0300

    Ejercicio 1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+
+        // retrieve configured instance
+        Saludo serviceSaludo = context.getBean("saludoBean", Saludo.class);
+
+        serviceSaludo.imprimirSaludo();
+    }
+
+}
---
+++ src/main/java/org/example/Saludo.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Saludo {
+    public static void imprimirSaludo() {
+        System.out.println("Saludo desde el Bean");
+    }
+}
